431dc72173ba22a04b71388c6a9417763fdb8acb
cdyap <cdyap@outlook.com>
2017-10-08 16:51:34 +0800

    Time settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,9 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.encoding = "utf-8"    
+  config.time_zone = 'Hong Kong'
+    
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
