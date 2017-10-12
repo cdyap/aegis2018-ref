113f2cbaa8b0f28d932bdc9a9b2e62639a077ce9
cdyap <cdyap@outlook.com>
2017-10-12 22:41:44 +0800

    updated URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Rails.application.configure do
 
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'http://photoshoot.aegis2018.com/'
+  host = 'http://aegis2018.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
