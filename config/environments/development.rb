e43e2350bdd1a8b8178192e79b8436688c42ffa9
cdyap <cdyap@outlook.com>
2017-10-14 12:18:14 +0800

    Updated email for password reset

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.time_zone = 'UTC'
+  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.asset_host = "http://localhost:3000"
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp://127.0.0.1:1025',
