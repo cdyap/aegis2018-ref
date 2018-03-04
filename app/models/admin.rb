f17dbe23e8c700832e6476e2e0e539f1458f16c4
cdyap <cdyap@outlook.com>
2018-03-04 13:35:12 +0800

    Admin login

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,5 +3,13 @@ class Admin < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  
+  def create_or_update
+    raise ReadOnlyRecord, "#{self.class} is marked as readonly" if readonly?
+    account = Admin.where(id: self.id).first
+    if account
+      self.id = account.id
+    end
+    result = new_record? ? _create_record : _update_record
+    result != false
+  end
 end
